85bedaf255b279ee23896012b8a39df79faa87fd
Meirion Hughes <crakinshot@yahoo.com>
2019-10-31 10:35:10 +0000

    fix: injectedInto type guard

--- packages/pigly/src/predicates/injected-into.ts
+++ packages/pigly/src/predicates/injected-into.ts
@@ -2,8 +2,8 @@ import { IContext } from "../_context";
 
 
 export function injectedInto(symbol: symbol): (ctx: IContext) => boolean
+export function injectedInto(): never;
 export function injectedInto<P>(): (ctx: IContext) => boolean;
-export function injectedInto<P extends unknown>(): void;
 export function injectedInto(symbol?: symbol): (ctx: IContext) => boolean {
   return (ctx: IContext) => ctx.parent && ctx.parent.target == symbol;
 }
